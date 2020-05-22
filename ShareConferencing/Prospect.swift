e0ae8270deb9634ff25696d4081e66d8ba8f0bc5
John Naylor <jonaylor89@gmail.com>
2020-05-21 21:01:40 -0400

    fin

---
+++ ShareConferencing/Prospect.swift
@@ -0,0 +1,50 @@
+//
+//  Prospect.swift
+//  ShareConferencing
+//
+//  Created by John Naylor on 5/20/20.
+//  Copyright © 2020 John Naylor. All rights reserved.
+//
+
+import SwiftUI
+
+class Prospect: Identifiable, Codable {
+    var id = UUID()
+    var name = "Anonymous"
+    var email = ""
+    fileprivate(set) var isContacted = false
+}
+
+class Prospects: ObservableObject {
+    @Published private(set) var people: [Prospect]
+    static let saveKey = "SavedData"
+    
+    init() {
+        if let data = UserDefaults.standard.data(forKey: Self.saveKey) {
+            if let decoded = try? JSONDecoder().decode([Prospect].self, from: data) {
+                self.people = decoded
+                
+                return
+            }
+        }
+        
+        self.people = []
+    }
+    
+    private func save() {
+        if let encoded = try? JSONEncoder().encode(people) {
+            UserDefaults.standard.set(encoded, forKey: Self.saveKey)
+        }
+    }
+    
+    func add(_ prospect: Prospect) {
+        people.append(prospect)
+        save()
+    }
+    
+    func toggle(_ propect: Prospect) {
+        objectWillChange.send()
+        propect.isContacted.toggle()
+        save()
+    }
+}
